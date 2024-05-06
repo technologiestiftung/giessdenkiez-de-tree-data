70d1f03e08cec0a44713becce1734322aa413a9d
ff6347 <hash@ff6347.email>
2024-05-06 17:10:33 +0200

    chore: Remoce unneeded conf file

--- src/get-wfs-data.ts
+++ src/get-wfs-data.ts
@@ -6,7 +6,7 @@ import { delay } from "./utils.js";
 // write to file
 
 export async function getWfsData() {
-	const spinner = ora("Getting WFS data").start();
+	const spinner = ora("Getting WFS data (not yet implemented)").start();
 	delay(2000);
 	spinner.text = "Getting WFS data";
 	spinner.succeed("WFS data retrieved");
